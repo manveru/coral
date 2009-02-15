6b27f34f2d182e38934164e4ccf17482b6ab599e
Michael Fellinger <m.fellinger@gmail.com>
2009-02-16 01:07:54 +0900

    Better dumping

--- lib/coral/index.rb
+++ lib/coral/index.rb
@@ -2,11 +2,16 @@ module Coral
   class Index < ::Hash
     attr_reader :reef, :file
 
-    def initialize(local_reef)
+    def initialize(local_reef, retrying = false)
       @reef = local_reef
       @file = Pathname(reef) + 'index.yaml'
 
-      replace YAML.load_file(file) if file.exist?
+      load!(file)
+    rescue ArgumentError => error
+      raise error if retrying
+      reindex!
+      retrying = true
+      retry
     end
 
     def to_hash
@@ -14,7 +19,8 @@ module Coral
     end
 
     def find_repo(repo_name)
-      key?(repo_name) and "#{repo_name}/#{self[repo_name].first}"
+      return unless key?(repo_name)
+      "#{repo_name}/#{self[repo_name].first}"
     end
 
     def add(remote)
@@ -23,13 +29,26 @@ module Coral
     end
 
     def dump!
-      File.open(file, 'w+'){|io| io.puts(self.to_hash.to_yaml) }
+      File.open(file, 'w+'){|io|
+        hash = {}
+        each{|k,v| hash[k.to_s] = v.map{|vv| vv.to_s }}
+        io.print(hash.to_yaml)
+      }
+    end
+
+    # seems like we discovered a bug in YAML...
+    def load!(file)
+      hash = {}
+      YAML.load_file(file).each do |k, v|
+        hash[Pathname(k)] = v.map{|vv| Pathname(vv) }
+      end
+      replace(hash)
     end
 
     def reindex
       index = {}
 
-      reef.chdir do
+      Dir.chdir(reef) do
         Pathname.glob('*/*').each do |dir|
           repo, branch = dir.split
           (index[repo] ||= []) << branch
